550e85839767f616ae2374ce199df8e044dfdd56
jeetiss <za5rubley@gmail.com>
2019-01-22 19:51:40 +0300

    move to typescript

--- rollup.config.js
+++ rollup.config.js
@@ -1,11 +1,13 @@
-import babel from 'rollup-plugin-babel'
+import typescript from 'rollup-plugin-typescript2'
 import pkg from './package.json'
 
 export default {
-  input: 'src/index.js',
+  input: 'src/index.ts',
   output: [
     { file: pkg.main, format: 'cjs' },
     { file: pkg.module, format: 'es' },
   ],
-  plugins: [babel()],
+  plugins: [
+    typescript(),
+  ],
 }
