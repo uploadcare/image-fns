2a1161fc330abf0c6697bab6cadd294846c0dac5
jeetiss <za5rubley@gmail.com>
2019-02-14 15:46:41 +0300

    move test to ts

--- karma.config.js
+++ karma.config.js
@@ -1,41 +1,31 @@
-const typescript = require('rollup-plugin-typescript2')
-const resolve = require('rollup-plugin-node-resolve')
-const commonjs = require('rollup-plugin-commonjs')
-const path = require('path')
-
 process.env.CHROME_BIN = require('puppeteer').executablePath()
 
 module.exports = function(config) {
   config.set({
     browsers: ['ChromeHeadless'],
-    frameworks: ['mocha'],
+    reporters: ['progress', 'karma-typescript'],
+    frameworks: ['mocha', 'karma-typescript'],
     singleRun: true,
 
-    files: [{ pattern: 'tests/**.js' }],
-
-    plugins: [
-      'karma-rollup-preprocessor',
-      'karma-chrome-launcher',
-      'karma-mocha',
+    files: [
+      {
+        pattern: 'src/**.ts',
+        watched: false,
+      },
+      {
+        pattern: 'test/**.ts',
+        watched: false,
+      },
     ],
 
-    preprocessors: {
-      'tests/**.js': ['rollup'],
-    },
+    plugins: ['karma-typescript', 'karma-chrome-launcher', 'karma-mocha'],
 
-    rollupPreprocessor: {
-      plugins: [
-        resolve({ browser: true }),
-        commonjs({
-          include: 'node_modules/**',
-          namedExports: { chai: ['expect'] },
-        }),
-        typescript(),
-      ],
-      output: {
-        format: 'iife',
-        name: 'imageFns',
-        sourcemap: false,
+    preprocessors: { '**/*.ts': ['karma-typescript'] },
+
+    karmaTypescriptConfig: {
+      compilerOptions: {
+        lib: ['esnext', 'dom'],
+        tsconfig: "./tsconfig.json"
       },
     },
   })
